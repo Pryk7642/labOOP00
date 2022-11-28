af4b5127d783ac3dcd5b642139ea31c5862204da
Pryk7642 <phiriyakorn.p@kkumail.com>
2022-11-28 16:13:27 +0700

    add name

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Phiriyakorn Phanphanich!");
+Console.WriteLine("653450098-0");
+Console.WriteLine("phiriyakorn.p@kkumail.com");
